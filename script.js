30ba4d39bd91f051ae8486067b38b4a4184b5e87
Pavel <pavelsuspicin@gmail.com>
2022-05-04 16:01:52 +0300

    04.05.22

--- script.js
+++ script.js
@@ -5,15 +5,11 @@ let index = 1
 let speed = 300 / speedEl.value
 
 writeText()
-
 function writeText() {
     textEl.innerText = text.slice(0, index)
-
     index++
 
-    if (index > text.length) {
-        index = 1
-    }
+    if (index > text.length) index = 1
     setTimeout(writeText, speed)
 }
 
